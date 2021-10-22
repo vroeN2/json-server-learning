37646f287a8a95088498a0433a79fb8b79f5ae7c
Maciej Zachariasz <47660926+vroeN2@users.noreply.github.com>
2021-10-22 08:38:37 +0200

    fetch data
    learn how to fetch data from JSON server

--- js/index.js
+++ js/index.js
@@ -1,1 +1,26 @@
 // javascript for index.html
+const container = document.querySelector('.blogs');
+
+const renderPosts = async () => {
+    let uri = 'http://localhost:3000/posts';
+
+    const res = await fetch(uri);
+    const posts = await res.json();
+    console.log(posts);
+
+    let template = '';
+    posts.forEach(post => {
+        template += `
+            <div class='post'>
+                <h2>${post.title}</h2>
+                <p><small>${post.likes} likes</small></p>
+                <p>${post.body.slice(0, 200)}</p>
+                <a href="/details.html">read more...</a>
+            </div>
+        `
+    })
+
+    container.innerHTML = template;
+}
+
+window.addEventListener('DOMContentLoaded', () => renderPosts());
